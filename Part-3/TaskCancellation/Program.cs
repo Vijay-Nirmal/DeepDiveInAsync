00ff564276b577d5603e00f20373d582faa7a026
Vijay Nirmal <pon.vijaynirmal@outlook.com>
2021-07-24 21:07:02 +0530

    Added sample for CancellationToken

---
+++ Part-3/TaskCancellation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskCancellation
+{
+    internal static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.CancelAfter(6000);
+
+            try
+            {
+                await Task.Delay(5000, cancellationTokenSource.Token);
+            }
+            catch (OperationCanceledException)  // Cancelled task MUST throw OperationCanceledException
+            {
+                Console.WriteLine("Task is cancelled");
+            }
+
+            Console.WriteLine("Application Ended");
+            // Console.ReadKey(); // To prevent console from exiting immediately
+        }
+    }
+}
